7e342e8b1041216e08f5e443a1f14e683a8c0948
MARU420 <ammar.wizzy@gmail.com>
2025-03-18 16:44:49 +0100

    assignment7 done

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -4,14 +4,21 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Assignment7 is ERC20 {
+    address owner;
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
-
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        owner = msg.sender;
+    }
+    modifier onlyOwner (address account){
+        require(account == owner);
+        _;
+    }
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint() {
+    function mint(address receiver, uint256 amount) public onlyOwner(msg.sender) {
+        _mint(receiver, amount);
         // Fill in the logic
         // call _mint function with to and amount params
     }
